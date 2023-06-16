b7a5b33b3beda1ad99beb6fad19e262668e65ded
sunilreddyg <sunilreddy.gajjala@gmail.com>
2023-06-16 11:43:22 +0530

    Add files via upload

---
+++ Project_213/src/mq/objectidentification/FB_Login.java
@@ -0,0 +1,20 @@
+package objectidentification;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FB_Login 
+{
+
+	public static void main(String[] args) {
+		
+		WebDriver driver=new ChromeDriver();
+		driver.get("http://facebook.com");
+		driver.manage().window().maximize();
+		
+		driver.findElement(By.id("email")).sendKeys("darshan");
+
+	}
+
+}
