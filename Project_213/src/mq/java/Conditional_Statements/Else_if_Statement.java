2c227af5462e2b3c0ff84796566b7ef87d82be79
sunilreddyg <sunilreddy.gajjala@gmail.com>
2023-06-27 11:57:45 +0530

    Add files via upload

---
+++ Project_213/src/mq/java/Conditional_Statements/Else_if_Statement.java
@@ -0,0 +1,48 @@
+package mq.java.Conditional_Statements;
+
+public class Else_if_Statement {
+
+	public static void main(String[] args) 
+	{
+		
+		
+		/*
+		 * Elseif:-->
+		 * 		Multiple decision take place here..
+		 */
+		
+		int month = 1;
+		if(month==1)
+		{
+			System.out.println("Jan");
+		}
+		else if (month==2) 
+		{
+			System.out.println("Feb");
+		}
+		else if (month==3) 
+		{
+			System.out.println("Mar");
+		}
+		else if(month==4)
+		{
+			System.out.println("Apr");
+		}
+		else
+		{
+			System.out.println("No Month is matching");
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ Project_213/src/mq/java/Conditional_Statements/If_Condition.java
@@ -0,0 +1,38 @@
+package mq.java.Conditional_Statements;
+
+public class If_Condition 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		/*
+		 * Note:-->
+		 * 		If is a single decision statement
+		 * 		Note:--> If accept only boolean value ture/false
+		 */
+		
+		
+		System.out.println("Statement1");
+		
+		int age=9;
+		if(age > 18)  //Greater than is operator it return boolean value
+		{
+			System.out.println("Statement2");
+			System.out.println("Statement3");
+		}
+	
+		
+		
+		System.out.println("Statement4");
+		
+		
+		
+		boolean flag=true;
+		if(flag)
+			System.out.println("true Condition");
+		
+
+	}
+
+}
