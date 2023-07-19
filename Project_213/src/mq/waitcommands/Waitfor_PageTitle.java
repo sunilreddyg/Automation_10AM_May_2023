0cd532c4e39206acce13c13c955da99ed3ff91f1
sunilreddyg <sunilreddy.gajjala@gmail.com>
2023-07-19 11:34:58 +0530

    Add files via upload

---
+++ Project_213/src/mq/waitcommands/Waitfor_PageTitle.java
@@ -0,0 +1,41 @@
+package waitcommands;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Waitfor_PageTitle 
+{
+
+	public static void main(String[] args) 
+	{
+	
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://facebook.com");
+		driver.manage().window().maximize();
+		
+		
+		WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(100));
+		wait.until(ExpectedConditions.titleIs("Facebook – log in or sign up"));
+		System.out.println("Facebook Page title verified");
+		
+		WebElement ForgotPassword=driver.findElement(By.linkText("Forgotten password?"));
+		ForgotPassword.click();
+		
+		try {
+			wait.until(ExpectedConditions.titleContains("Forgotten"));
+			System.out.println("Forgot Password verified");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		driver.close();
+		
+	}
+
+}
